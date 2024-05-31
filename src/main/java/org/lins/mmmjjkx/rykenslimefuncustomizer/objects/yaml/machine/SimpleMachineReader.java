a0cda16c9b06a3dfe910e00a852e6a97bec0527b
Ddggdd135 <1306334428@qq.com>
2024-05-31 19:07:40 +0800

    chore(code style): do not use star-import

--- src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/yaml/machine/SimpleMachineReader.java
+++ src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/yaml/machine/SimpleMachineReader.java
@@ -4,7 +4,6 @@ import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.implementation.items.electric.machines.*;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.collections.Pair;
 import java.util.List;
 import org.bukkit.configuration.ConfigurationSection;
