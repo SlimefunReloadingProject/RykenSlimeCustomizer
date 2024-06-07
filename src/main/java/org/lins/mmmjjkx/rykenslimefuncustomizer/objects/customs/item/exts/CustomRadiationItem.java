457ae76c56a108ec6393314e6213cb2eab7e6e8b
lijinhong11 <tygfhk@outlook.com>
2024-06-07 20:42:09 +0800

    fix: 辐射物品没有脚本

--- src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/item/exts/CustomRadiationItem.java
+++ src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/item/exts/CustomRadiationItem.java
@@ -1,10 +1,12 @@
 package org.lins.mmmjjkx.rykenslimefuncustomizer.objects.customs.item.exts;
 
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.Radioactive;
 import io.github.thebusybiscuit.slimefun4.core.attributes.Radioactivity;
+import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.lins.mmmjjkx.rykenslimefuncustomizer.objects.customs.parent.BaseRadiationItem;
@@ -24,6 +26,16 @@ public class CustomRadiationItem extends BaseRadiationItem implements Radioactiv
         super(itemGroup, radioactivity, item, recipeType, recipe);
 
         this.radioactivity = radioactivity;
+        if (eval != null) {
+            eval.doInit();
+
+            this.addItemHandler((ItemUseHandler) e -> {
+                eval.evalFunction("onUse", e, this);
+                e.cancel();
+            });
+        } else {
+            this.addItemHandler((ItemUseHandler) PlayerRightClickEvent::cancel);
+        }
 
         constructorArgs = new Object[] {itemGroup, item, recipeType, recipe, radioactivity};
     }
