a1f3573c4c9b6217157c3bbeff186fe58b06c168
lijinhong11 <tygfhk@outlook.com>
2024-09-06 23:28:05 +0800

    fix

--- src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/CustomAddonConfig.java
+++ src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/CustomAddonConfig.java
@@ -1,13 +1,11 @@
 package org.lins.mmmjjkx.rykenslimefuncustomizer.objects.customs;
 
-import lombok.Getter;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.Nullable;
 import org.lins.mmmjjkx.rykenslimefuncustomizer.objects.script.ScriptEval;
 
 import java.io.File;
 
-@Getter
 public record CustomAddonConfig(File configFile, YamlConfiguration config, ScriptEval onReloadHandler) {
     public CustomAddonConfig(File configFile, YamlConfiguration config, @Nullable ScriptEval onReloadHandler) {
         this.configFile = configFile;
