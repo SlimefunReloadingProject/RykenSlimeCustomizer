0bd728a7adc49f18e23d9b5ecfb5158da19651f6
balugaq <129668183+balugaq@users.noreply.github.com>
2025-03-29 10:38:00 +0800

    增加注释 (#60)

--- src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/machine/MachineInfo.java
+++ src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/machine/MachineInfo.java
@@ -10,6 +10,38 @@ import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.Nullable;
 import org.lins.mmmjjkx.rykenslimefuncustomizer.objects.customs.machine.CustomMachine;
 
+/**
+ * This data class provides access to machine data for JavaScript integration.
+ *
+ * @param blockMenu the menu of the machine, may be null if the machine has no GUI.
+ * @param data the persistent data of the machine.
+ * @param machineItem the {@link SlimefunItem} instance of the machine.
+ * @param block the physical {@link Block} instance of the machine.
+ * @param processor the {@link MachineProcessor} handling the machine's logic.
+ * @param operation the current {@link MachineOperation}, ALWAYS be null so far...
+ * @param machine the {@link CustomMachine} instance defining the machine's behavior.
+ * @author lijinhong11
+ *
+ * <h3>JavaScript Usage Example:</h3>
+ * <pre>{@code
+ * function tick(info) {
+ *     // May be null
+ *     var menu = info.blockMenu();
+ *     // Access Java fields in JavaScript should like this:
+ *     // var yourObjectName = info.<recordName>();
+ *     // For example:
+ *     // var block = info.block();
+ *
+ *     if (menu !== null) {
+ *         // See Slimefun API for more details
+ *         var item = blockMenu.getItemInSlot(0);
+ *     }
+ *
+ *     // Displayed name
+ *     var itemName = info.machineItem().getItemName();
+ * }
+ * }</pre>
+ */
 public record MachineInfo(
         @Nullable BlockMenu blockMenu,
         SlimefunBlockData data,
